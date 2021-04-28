82852b6d78a94772d415bdae72e230ac449f081e
Rune Juhl-Petersen <rune@juhl-petersen.net>
2021-04-29 00:04:34 +0200

    reverse proxy initial implementation

---
+++ websocket-proxy/gateway.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+handlers = {}
+
+app.get('/', function (req, res) {
+	res.send('ok');
+});
+
+app.get('/*', function (req,res){
+    var handler = handlers[req.path];
+    if(handler){
+        handler(req,res);
+    }else{
+        res.send('no handler');
+    }
+})
+
+io.on('connection', function (socket) {
+    var request_id=1;
+    responses = {}
+    var game=undefined;
+	socket.on('create', function (msg) {
+        //register handler for endpoint
+        //define handler
+        var path= msg.path
+        console.log('creating a listener for ' + path);
+        handlers[path]= (req,res)=>{
+            //create id for request
+            var rid= request_id++;
+            responses[rid] = res;
+            socket.emit('request',{ rid: rid, headers:"",body:""})
+            console.log(`request ${rid} processed`);
+        }
+	});
+
+	socket.on('response', function (msg) {
+        rhandler = responses[msg.rid]
+        if(rhandler){
+            console.log(`returning response ${msg.rid}`)
+            rhandler.send(msg.body);
+            responses[msg.id]=null;
+        }else{
+            console.log(`response ${msg.rid} could not be returned`)
+        }
+	});
+});
+
+http.listen(3000,function(){
+	console.log('listening on *:3000');
+});
---
+++ websocket-proxy/proxy.js
@@ -0,0 +1,29 @@
+const io = require("socket.io-client");
+const socket = io("ws://localhost:3000");
+
+
+
+
+
+socket.on("connect", () => {
+  // either with send()
+  var path =process.argv.length>2?`/${process.argv[2]}`: `/noget`
+    socket.emit("create", {path:path})
+    console.log('connected')
+});
+
+// handle the event sent with socket.send()
+socket.on("request", data => {
+  console.log(data);
+  setTimeout(() => {
+      var body = process.argv.length>3 ? process.argv[3] : 'juhuu'
+    socket.emit("response", {
+            rid:data.rid,  
+            body:body + data.rid
+        }
+    );
+  }, (000));
+  
+});
+
+
